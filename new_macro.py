a3a38df02ff778160fdbe1e6ee8018e0d625771c
kyo <dmsry060209@gmail.com>
2025-01-06 15:08:52 +0900

    Update new_macro.py

--- new_macro.py
+++ new_macro.py
@@ -3,6 +3,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support.ui import WebDriverWait
 import time
 import os
 
